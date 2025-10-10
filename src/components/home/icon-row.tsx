5606f9241a5f6d44b67c2585b141eea5e6c1d75b
Natalie Bridgers <natalie@iori.local>
2025-10-09 19:38:28 -0500

    foooter

--- src/components/home/icon-row.tsx
+++ src/components/home/icon-row.tsx
@@ -7,7 +7,7 @@ import {
   FaLastfm,
 } from "react-icons/fa6";
 
-export function IconRow() {
+export function IconRow({ smaller }: { smaller?: boolean }) {
   let icons = [
     { i: <FaGithub />, url: "https://github.com/espeon" },
     { i: <FaBluesky />, url: "https://bsky.app/profile/natalie.sh" },
@@ -16,13 +16,13 @@ export function IconRow() {
     { i: <FaDiscord />, url: "https://discord.gg/pgGM9n8ppf" },
   ];
   return (
-    <div className="flex py-2" style={{ fontSize: "1.75rem" }}>
+    <div
+      className={`flex gap-2 py-2`}
+      style={{ fontSize: smaller ? "1.3125rem" : "1.75rem" }}
+    >
       {icons.map((e) => {
         return (
-          <div
-            className="pr-[.25em] text-gray-700 dark:text-gray-400"
-            key={e.url}
-          >
+          <div className="text-gray-700 dark:text-gray-400" key={e.url}>
             <a href={e.url}>{e.i}</a>
           </div>
         );
