9a8ae14c4c16b859a15be995f9db720b658d66b6
Natalie B. <22222885+espeon@users.noreply.github.com>
2025-12-02 21:38:59 -0600

    little smoother

--- src/components/home/lastfm.tsx
+++ src/components/home/lastfm.tsx
@@ -2,6 +2,7 @@
 import { useLastFM } from "../../hooks/useLastFM";
 import { useEffect } from "react";
 import { setBackgroundState } from "../../hooks/useBackgroundState";
+import { CrossFade } from "react-crossfade-simple";
 
 export function LastFM() {
   const { data, error, loading } = useLastFM(10000);
@@ -15,34 +16,49 @@ export function LastFM() {
   }, [data?.imageUrl, data]);
 
   return (
-    <div className="text-gray-600 dark:text-gray-300">
-      {data ? (
-        <span>
-          I like music a considerable amount.{" "}
-          {data.isCurrent ? "Right now, I'm playing" : "I recently played"}{" "}
-          <a
-            href={`https://www.last.fm/music/${data.artist}/_/${data.name}`}
-            target="_blank"
-            className="hover:text-gray-800 dark:hover:text-gray-100"
-          >
-            {data.name}
-          </a>{" "}
-          by{" "}
-          <a
-            href={`https://www.last.fm/music/${data.artist}/`}
-            target="_blank"
-            className="hover:text-gray-900 dark:hover:text-gray-200"
-          >
-            {data.artist}
-          </a>
-          . The background is based on the album art from what{" "}
-          {data.isCurrent ? "I'm listening to" : "I've last listened to"}.
-        </span>
-      ) : error ? (
-        <span>{error.message}</span>
-      ) : (
-        <span>loading...</span>
-      )}
-    </div>
+    <CrossFade
+      contentKey={
+        (data?.isCurrent || "b all") +
+        (data?.artist || "abs") +
+        (data?.name || "balls")
+      }
+    >
+      <div className="min-h-[7.3rem] text-gray-600 sm:min-h-[5.3rem] dark:text-gray-300">
+        {data ? (
+          <span>
+            I like music a considerable amount.{" "}
+            {data.isCurrent ? "Right now, I'm playing" : "I recently played"}{" "}
+            <a
+              href={`https://www.last.fm/music/${data.artist}/_/${data.name}`}
+              target="_blank"
+              className="hover:text-gray-800 dark:hover:text-gray-100"
+            >
+              {data.name}
+            </a>{" "}
+            by{" "}
+            <a
+              href={`https://www.last.fm/music/${data.artist}/`}
+              target="_blank"
+              className="hover:text-gray-900 dark:hover:text-gray-200"
+            >
+              {data.artist}
+            </a>
+            .{" "}
+            <a
+              href="/lfm"
+              className="font-normal no-underline transition-colors hover:text-sky-700 dark:hover:text-sky-200"
+            >
+              The background
+            </a>{" "}
+            is based on the album art from what{" "}
+            {data.isCurrent ? "I'm listening to" : "I've last listened to"}.
+          </span>
+        ) : error ? (
+          <span>{error.message}</span>
+        ) : (
+          <span>I like music a considerable amount.</span>
+        )}
+      </div>
+    </CrossFade>
   );
 }
